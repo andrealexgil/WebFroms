c0a4d39e9dd7608258eac04c8bbcf6de25a3e05a
andrealexgil <50157824+andrealexgil@users.noreply.github.com>
2020-02-28 19:45:18 +0000

    start using ajax

---
+++ WebSite1/Account/Login.aspx.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+using System.Web;
+using System.Web.UI;
+using WebSite1;
+
+public partial class Account_Login : Page
+{
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void LogIn(object sender, EventArgs e)
+        {
+
+        }
+        
+        [System.Web.Services.WebMethod(EnableSession = true)]
+        public static string Hi()
+        {
+            return "Hello World";
+        } 
+}
---
+++ WebSite1/Model/JASONRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for JASONRequest
+/// </summary>
+public class JASONRequest
+{   
+        public int id { get;set; }
+        public string name { get; set;}
+        public bool isComplete { get; set; }
+
+    public  void ToString() => Console.Write("hi :)");
+}
